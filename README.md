9ef77db9fcdcfb3a3db170c0bd113ec986100020
Juan Francisco Herrero <juanfraherrero@gmail.com>
2024-06-28 16:13:55 -0300

    Add Workflow CI/CD by Github Actions to check linting and style, and create realese for linux, windows, and macos

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Images are distributes as columns within names.
 
 Use it for your needs.
 
-## How ti use it
+## How to use it
 
 Clone the repo, then:
 
@@ -21,10 +21,16 @@ python main.py
 
 Or can use the ui:
 ``` bash
-python ui.py
+python app.py
 ```
 
-Also can run the executable with linux
-``` bash
-./dist/ui
-```
+Also you can download the executable for your OS (Linux(debian based), macOS, Windows) in the realese section
+
+## Motivation (28/06/2024)
+I'm currently writing my thesis for my Software Engineering career, and when I have to compare some images, I cannot add and composite them in columns for comparison. So, I created this app to solve that problem and also to reinforce my knowledge in GitHub Actions, specifically in CI/CD pipelines and releases.
+
+## App previews
+![Preview of the app](images/app_preview.png)
+![Another preview of the app](images/app_preview2.png)
+## Example results
+![Example of the result](images/example_result.jpg)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from PIL import Image, ImageDraw, ImageFont
 import yaml
+from PIL import Image, ImageDraw, ImageFont
 
-### NO CAMBIAR
 # Cargar el archivo YAML
-with open('config.yaml', 'r') as file:
-  config = yaml.safe_load(file)
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
 
 # Función para recortar la imagen al centro
 def crop_center(image, crop_size):
@@ -16,13 +16,19 @@ def crop_center(image, crop_size):
     bottom = (height + new_height) / 2
     return image.crop((left, top, right, bottom))
 
-images = [[crop_center(Image.open(image_path), (600,600)) for image_path in image_paths] for image_paths in config["images"]]
+
+images = [
+    [crop_center(Image.open(image_path), (600, 600)) for image_path in row]
+    for row in config["images"]
+]
 
 # Asumimos que todas las imágenes tienen el mismo tamaño
 width, height = config["size"], config["size"]
 
 cant_columns = len(images)
-cant_rows =  len(max(images, key=len)) #obtener la mayopr cantidad de imagenes en una columnas
+cant_rows = len(
+    max(images, key=len)
+)  # obtener la mayopr cantidad de imagenes en una columnas
 
 # Definir espacio entre imágenes y altura para los nombres de las columnas
 spacing = 10
@@ -30,8 +36,8 @@ column_name_height = 64
 
 # Crear una nueva imagen con el tamaño adecuado
 total_width = (width + spacing) * cant_columns + spacing
-total_height = (height+spacing) * cant_rows + spacing + column_name_height
-new_image = Image.new('RGB', (total_width, total_height), color='white')
+total_height = (height + spacing) * cant_rows + spacing + column_name_height
+new_image = Image.new("RGB", (total_width, total_height), color="white")
 
 # Crear objeto de dibujo
 draw = ImageDraw.Draw(new_image)
@@ -49,16 +55,32 @@ column_names = config["column_names"]
 print("total size", total_width, total_height)
 
 # Dibujar los nombres de las columnas
-for index, column  in enumerate(column_names):
-  draw.text(((width+spacing)*(index) + (width // 2) - draw.textsize(column, font=font)[0] // cant_columns, 0), column, fill="black", font=font) 
+for index, column in enumerate(column_names):
+    draw.text(
+        (
+            (width + spacing) * (index)
+            + (width // 2)
+            - draw.textsize(column, font=font)[0] // cant_columns,
+            0,
+        ),
+        column,
+        fill="black",
+        font=font,
+    )
 
 # Pegar las imágenes en la nueva imagen
 for i in range(cant_rows):
-  for j in range(cant_columns):
-     new_image.paste(images[j][i], (j * (width + spacing) + spacing, column_name_height + spacing + i * (height + spacing)))
+    for j in range(cant_columns):
+        new_image.paste(
+            images[j][i],
+            (
+                j * (width + spacing) + spacing,
+                column_name_height + spacing + i * (height + spacing),
+            ),
+        )
 
 # Guardar la nueva imagen
-new_image.save(config["output"]+'.jpg')
+new_image.save(config["output"] + ".jpg")
 
 # Mostrar la nueva imagen (opcional)
 new_image.show()
